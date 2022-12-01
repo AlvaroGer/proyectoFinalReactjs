2d638ccea61a7be4fcac0ab440c753184f234898
Alvaro Martinez <alvaro.g.martinez.rodriguez@gmail.com>
2022-11-30 23:21:53 -0300

    Proyecto Final

---
+++ src/components/Promocion.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Promocion = () => {
+    return (
+        <div className="row">
+            <div className="col-md-6">
+                <img src={"img/BlackFriday.png"} alt={"Pc Victus"} />
+            </div>
+            <div className="col-md-6">
+                <h3>Disfruta de nuestro Balck Friday pero con descuentos exclusivos durante todo el año en Norebooks</h3>
+                <p><a href={"./notebooks"} target={"_blank"} img src={"img/1-158.jpg"}></a> </p>
+            </div>
+        </div>
+    )
+}
+
+export default Promocion;
